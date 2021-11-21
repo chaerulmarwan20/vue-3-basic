d11bbf1c0d7fd5af782e1ed528276023f8dc5036
Marwan <chaerul.marwan@weekendinc.com>
2021-11-21 10:52:15 +0700

    compouted

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
-  <div v-html="message" />
+  <div>{{ total }}</div>
+  <input type="text" v-model="num1" />
+  <input type="text" v-model="num2" />
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: "<span style='color: blue'>Biru</span>",
+      num1: 0,
+      num2: 0,
     };
+  },
+  computed: {
+    total() {
+      return parseInt(this.num1) + parseInt(this.num2);
+    },
   },
 };
 </script>
