f27edecae2ed41c520c66134bd20107f86b18eac
Marwan <chaerul.marwan@weekendinc.com>
2021-11-21 09:48:50 +0700

    component & props

---
+++ src/components/ChildComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>Child Component</div>
+  <div>{{ text }}</div>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
