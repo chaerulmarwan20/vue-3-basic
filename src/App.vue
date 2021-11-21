79f3945a0a51254b2ceedda72792e1f3868c6530
Marwan <chaerul.marwan@weekendinc.com>
2021-11-21 12:17:19 +0700

    global component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,7 @@
 <template>
-  <child-component>
-    <!-- without destructuring object -->
-    <template v-slot:default="props">
-      <span>Judul : {{ props.data }}</span>
-    </template>
-    <template v-slot:slotContent="props">
-      <span>Kontent : {{ props.data }}</span>
-    </template>
-    <!-- with destructuring object -->
-    <template v-slot:slotFooter="{ data }">
-      <span>Footer : {{ data }}</span>
-    </template>
-  </child-component>
+  <child-component />
 </template>
 
 <script>
-import ChildComponent from "./components/ChildComponent.vue";
-
-export default {
-  components: { ChildComponent },
-};
+export default {};
 </script>
--- src/components/ChildComponent.vue
+++ src/components/ChildComponent.vue
@@ -1,20 +1,7 @@
 <template>
-  <div>Title</div>
-  <slot :data="title" />
-  <div>Content</div>
-  <slot name="slotContent" :data="content" />
-  <div>Footer</div>
-  <slot name="slotFooter" :data="footer" />
+  <div>Child Component</div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      title: "Hello World",
-      content: "I'm Content",
-      footer: "Footer Here",
-    };
-  },
-};
+export default {};
 </script>
