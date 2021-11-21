fc5ca04c137ff5e9e057d5441a3f7825f4040162
Marwan <chaerul.marwan@weekendinc.com>
2021-11-21 08:47:55 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>{{ count }}</div>
+  <button @click="count++">Add 1</button>
+  <button @click="addCount">Add Count</button>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      count: 0,
+    };
+  },
+  methods: {
+    addCount() {
+      this.count += 2;
+    },
+  },
+};
+</script>
