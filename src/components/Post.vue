380279a3d86ecfcf7e065a3027a7ce6355e34bb9
Marwan <chaerul.marwan@weekendinc.com>
2021-11-21 17:22:00 +0700

    keep-alive

---
+++ src/components/Post.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="demo-tab">
+    <div v-for="(post, idx) in posts" :key="idx">
+      <span>{{ post.name }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      posts: [],
+    };
+  },
+  async created() {
+    const res = await fetch("https://swapi.dev/api/planets");
+    const { results } = await res.json();
+    this.posts = results;
+  },
+};
+</script>
