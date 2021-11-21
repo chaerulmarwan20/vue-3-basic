f32a24f5cf43df64fd22a1dc6e5a556c526282a7
Marwan <chaerul.marwan@weekendinc.com>
2021-11-21 09:48:18 +0700

    component and props

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,18 @@
 <template>
-  <ol>
-    <li v-for="(todo, idx) in todos" :key="todo.id">
-      {{ idx }} - {{ todo.activity }}
-    </li>
-  </ol>
+  <div>Parent Component</div>
+  <child-component text="Hello World" />
+  <child-component :text="message" />
 </template>
 
 <script>
+import ChildComponent from "./components/ChildComponent.vue";
+
 export default {
   data() {
     return {
-      todos: [
-        { id: 1, activity: "Futsal" },
-        { id: 2, activity: "Badminton" },
-        { id: 3, activity: "Basket" },
-      ],
+      message: "From Message",
     };
   },
+  components: { ChildComponent },
 };
 </script>
